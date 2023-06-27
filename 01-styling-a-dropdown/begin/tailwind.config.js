833eaf4d7e8c69e63d59aa9da0804b3a043d48ff
Sam Selikoff <sam.selikoff@gmail.com>
2023-06-27 06:23:15 -0400

    wip

--- 01-styling-a-dropdown/begin/tailwind.config.js
+++ 01-styling-a-dropdown/begin/tailwind.config.js
@@ -15,31 +15,6 @@ module.exports = {
         ...green,
         ...blackA,
       },
-      // keyframes: {
-      //   overlayShow: {
-      //     from: { opacity: 0 },
-      //     to: { opacity: 1 },
-      //   },
-      //   overlayHide: {
-      //     from: { opacity: 1 },
-      //     to: { opacity: 0 },
-      //   },
-      //   contentShow: {
-      //     from: { opacity: 0, transform: "translate(-50%, -50%) scale(.95)" },
-      //     to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
-      //   },
-      //   contentHide: {
-      //     to: { opacity: 0, transform: "translate(-50%, -50%) scale(.95)" },
-      //     from: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
-      //   },
-      // },
-      // animation: {
-      //   // overlayShow: "overlayShow 300ms cubic-bezier(0.16, 1, 0.3, 1)",
-      //   overlayShow: "overlayShow 300ms ease-out",
-      //   overlayHide: "overlayHide 200ms ease-in",
-      //   contentShow: "contentShow 300ms ease-out",
-      //   contentHide: "contentHide 200ms ease-in",
-      // },
     },
   },
   plugins: [require("@tailwindcss/forms")],

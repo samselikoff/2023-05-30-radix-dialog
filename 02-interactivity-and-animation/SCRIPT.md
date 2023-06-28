cb73ac1d8c8d45be32e169246c4f71c14c718ab1
Sam Selikoff <sam.selikoff@gmail.com>
2023-06-28 15:07:52 -0400

    wip

--- 02-interactivity-and-animation/SCRIPT.md
+++ 02-interactivity-and-animation/SCRIPT.md
@@ -1,3 +1,26 @@
 ```jsx
 useContacts()
 ```
+
+create a ContactCard
+
+
+rename UserFields, use Contact type
+
+wrap in form, handleSubmit
+
+```tsx
+async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+
+  const attrs = Object.fromEntries(new FormData(event.currentTarget));
+
+  await updateContact(contact.id, attrs);
+}
+```
+
+Controlled
+
+```tsx
+<Dialog.Root open={open} onOpenChange={setOpen}>
+```
--- 02-interactivity-and-animation/index/app/page.tsx
+++ 02-interactivity-and-animation/index/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { useContacts } from "@/lib/contacts";
+import { Contact, useContacts } from "@/lib/contacts";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross1Icon, Pencil1Icon } from "@radix-ui/react-icons";
+import { FormEvent, useState } from "react";
+import { Spinner } from "./spinner";
 
 export default function Page() {
   let { contacts } = useContacts();
@@ -11,61 +13,77 @@ export default function Page() {
     <div className="py-10">
       <div className="mx-auto max-w-sm space-y-4 rounded-lg bg-gray-200 p-4">
         {contacts.map((contact) => (
-          <div
-            className="flex justify-between rounded-lg bg-white px-4 py-4 text-gray-900 shadow"
-            key={contact.id}
-          >
-            <div>
-              <p>{contact.name}</p>
-              <p className="text-sm text-gray-500">{contact.role}</p>
-              <p className="text-sm text-gray-500">{contact.email}</p>
-            </div>
-            <div>
-              <Dialog.Root>
-                <Dialog.Trigger className="rounded p-2 hover:bg-gray-200">
-                  <Pencil1Icon />
-                </Dialog.Trigger>
-
-                <Dialog.Portal>
-                  <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-                  <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow">
-                    <div className="flex items-center justify-between">
-                      <Dialog.Title className="text-xl">
-                        Edit contact
-                      </Dialog.Title>
-                      <Dialog.Close className="text-gray-400 hover:text-gray-500">
-                        <Cross1Icon />
-                      </Dialog.Close>
-                    </div>
-
-                    <div className="mt-8">
-                      <UserFields user={contact} />
-                    </div>
-
-                    <div className="mt-8 space-x-6 text-right">
-                      <Dialog.Close className="rounded px-4 py-2 text-sm font-medium text-gray-500 hover:text-gray-600">
-                        Cancel
-                      </Dialog.Close>
-                      <button className="rounded bg-green-500 px-4 py-2 text-sm font-medium text-white hover:bg-green-600">
-                        Save
-                      </button>
-                    </div>
-                  </Dialog.Content>
-                </Dialog.Portal>
-              </Dialog.Root>
-            </div>
-          </div>
+          <ContactCard contact={contact} key={contact.id} />
         ))}
       </div>
     </div>
   );
 }
 
-function UserFields({
-  user,
-}: {
-  user: { id: string; name: string; email: string; role: string };
-}) {
+function ContactCard({ contact }: { contact: Contact }) {
+  let [open, setOpen] = useState(false);
+  let [saving, setSaving] = useState(false);
+  let { updateContact } = useContacts();
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setSaving(true);
+
+    const attrs = Object.fromEntries(new FormData(event.currentTarget));
+
+    await updateContact(contact.id, attrs);
+    setOpen(false);
+  }
+
+  return (
+    <div
+      className="flex justify-between rounded-lg bg-white px-4 py-4 text-gray-900 shadow"
+      key={contact.id}
+    >
+      <div>
+        <p>{contact.name}</p>
+        <p className="text-sm text-gray-500">{contact.role}</p>
+        <p className="text-sm text-gray-500">{contact.email}</p>
+      </div>
+      <div>
+        <Dialog.Root open={open} onOpenChange={setOpen}>
+          <Dialog.Trigger className="rounded p-2 hover:bg-gray-200">
+            <Pencil1Icon />
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+            <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow">
+              <div className="flex items-center justify-between">
+                <Dialog.Title className="text-xl">Edit contact</Dialog.Title>
+                <Dialog.Close className="text-gray-400 hover:text-gray-500">
+                  <Cross1Icon />
+                </Dialog.Close>
+              </div>
+
+              <form onSubmit={handleSubmit}>
+                <div className="mt-8">
+                  <ContactFields contact={contact} />
+                </div>
+                <div className="mt-8 space-x-6 text-right">
+                  <Dialog.Close className="rounded px-4 py-2 text-sm font-medium text-gray-500 hover:text-gray-600">
+                    Cancel
+                  </Dialog.Close>
+                  <button className="rounded bg-green-500 px-4 py-2 text-sm font-medium text-white hover:bg-green-600">
+                    <Spinner className="h-4 group-enabled:opacity-0" />
+                    Save
+                  </button>
+                </div>
+              </form>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </div>
+    </div>
+  );
+}
+
+function ContactFields({ contact }: { contact: Contact }) {
   return (
     <div className="space-y-6">
       <div>
@@ -74,7 +92,8 @@ function UserFields({
           autoFocus
           className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
           type="text"
-          defaultValue={user.name}
+          defaultValue={contact.name}
+          name="name"
         />
       </div>
 
@@ -85,7 +104,8 @@ function UserFields({
         <input
           className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
           type="text"
-          defaultValue={user.role}
+          defaultValue={contact.role}
+          name="role"
         />
       </div>
       <div>
@@ -95,7 +115,8 @@ function UserFields({
         <input
           className="mt-2 block w-full rounded-md border border-gray-300 px-2 py-1.5 text-sm text-gray-900 shadow-sm sm:leading-6"
           type="text"
-          defaultValue={user.email}
+          defaultValue={contact.email}
+          name="email"
         />
       </div>
     </div>

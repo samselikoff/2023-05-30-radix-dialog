56e221e7f18a650950d21ccfd847e10a498042f4
Sam Selikoff <sam.selikoff@gmail.com>
2023-07-17 14:27:25 -0400

    wip

---
+++ 03-reusable-component/index/app/modal.tsx
@@ -0,0 +1,67 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { ReactNode } from "react";
+
+export default function Modal({
+  open,
+  onOpenChange,
+  children,
+}: {
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+  children: ReactNode;
+}) {
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      {children}
+    </Dialog.Root>
+  );
+}
+
+function ModalButton({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className: string;
+}) {
+  return <Dialog.Trigger className={className}>{children}</Dialog.Trigger>;
+}
+
+function ModalContent({
+  children,
+  title,
+}: {
+  title: string;
+  children: ReactNode;
+}) {
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay className="fixed inset-0 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
+      <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-gray-900 shadow data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms]">
+        <div className="flex items-center justify-between">
+          <Dialog.Title className="text-xl">{title}</Dialog.Title>
+          <Dialog.Close className="text-gray-400 hover:text-gray-500">
+            <Cross1Icon />
+          </Dialog.Close>
+        </div>
+
+        {children}
+      </Dialog.Content>
+    </Dialog.Portal>
+  );
+}
+
+function ModalClose({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className: string;
+}) {
+  return <Dialog.Close className={className}>{children}</Dialog.Close>;
+}
+
+Modal.Button = ModalButton;
+Modal.Content = ModalContent;
+Modal.Close = ModalClose;
